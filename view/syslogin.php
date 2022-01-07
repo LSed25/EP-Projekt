eb1b71280f850c56085b0b9f8659fecf0dd84d69
Tjaša Mlinarič <tjasamlinaric.123@gmail.com>
2022-01-07 17:22:36 +0100

    admin+prodajalec prijava s cert

--- view/syslogin.php
+++ view/syslogin.php
@@ -4,24 +4,29 @@
         <title>Sistemska prijava</title>
     </head>
     <body>
+        <h1>Sistemska prijava</h1>
+
         <?php
         $authorized_users = ["admin", "prodaja"];
 
         $client_cert = filter_input(INPUT_SERVER, "SSL_CLIENT_CERT");
         $cert_data = openssl_x509_parse($client_cert);
-        
-        if(!$cert_data) {
-            http_response_code(403);
-            die('Forbidden');
-        }else{
 
+        $role = $cert_data["subject"]["role"];
+        
+        if($role == "administrator" || $role == "prodajalec") { ?>
+            <form action="<?= BASE_URL . "store/login" ?>" method="post">
+               <p><label>E-naslov: <input type="email" name="email" value="<?= $email ?>" /></label></p>
+               <p><label>Geslo: <input type="password" name="password" value="<?= $password ?>" /></label></p>
+               <input type="hidden" name="role" value="<?= $role ?>">
+               <p><button>Prijava</button></p>
+            </form>
+        <?php 
+        }
+        else { ?>
+            <p>Uporabnik ni avtoriziran.</p>
+        <?php
+        }       
         ?>
-        <h1>Sistemska prijava</h1>
-        <form action="<?= BASE_URL . "store/login" ?>" method="post">
-            <p><label>E-naslov: <input type="email" name="email" value="<?= $email ?>" /></label></p>
-            <p><label>Geslo: <input type="password" name="password" value="<?= $password ?>" /></label></p>
-            <p><button>Prijava</button></p>
-        </form>
-        <?php } ?>
     </body>
 </html>
